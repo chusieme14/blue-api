ba4c437bfd38d2df627708e3fda63b4107baf37e
paul <paul.anderson.b.garcia@gmail.com>
2025-07-31 15:31:12 +0800

    change api

--- app/Console/Commands/GetBouyData.php
+++ app/Console/Commands/GetBouyData.php
@@ -30,16 +30,16 @@ class GetBouyData extends Command
      */
     public function handle()
     {
-        $limit = 10;
-        $response = Http::timeout(1800)->get('http://44.245.234.197:4321/export_data/?user_id=1286&user_mobile=%2B33603328977&skip=0&limit=' . $limit);
+        $limit = 100;
+        $response = Http::timeout(1800)->get('http://export.blueoceangear.com:4321/api/v1/export_data/?user_id=111&user_mobile=%2B14158478829&skip=0&limit=' . $limit);
         // $pagination_data = DB::table('paginations')->select('skip')->first();
         $skip = 0;
         $counter = 0;
         $total_data = $response->json()['total'];
-        $total_page = ceil($total_data/10);
+        $total_page = ceil($total_data/100);
 
         for ($i=1; $i <= $total_page; $i++) {
-            $datas = Http::timeout(1800)->get('http://44.245.234.197:4321/export_data/?user_id=1286&user_mobile=%2B33603328977&skip=' . $skip . '&limit=' . $limit);
+            $datas = Http::timeout(1800)->get('http://export.blueoceangear.com:4321/api/v1/export_data/?user_id=111&user_mobile=%2B14158478829&skip=' . $skip . '&limit=' . $limit);
             foreach ($datas->json()['data'] as $key => $data) {
                 
                 $data['transmit_time'] = Carbon::parse($data['transmit_time']);
